bf4a6b7e965ed07ae3794ddb7a3dc151d00b8cdd
vishaldhakal <vishaldhakal96@gmail.com>
2024-09-19 14:46:34 +0545

    sdv

--- activity/serializers.py
+++ activity/serializers.py
@@ -121,7 +121,7 @@ class ActivitySmallSerializer(serializers.ModelSerializer):
     enquiries = ActivityEnquirySerializer(many=True,read_only=True)
     class Meta:
         model = Activity
-        fields = ('id','slug', 'activity_title', 'activity_category','enquiries','location','duration','price','coverImg','ratings','popular','best_selling','destination','activity_region','priceSale')
+        fields = ('id','slug', 'activity_title', 'activity_category','enquiries','location','duration','price','coverImg','ratings','popular','best_selling','destination','activity_region','priceSale','youtube_link')
         depth = 1
 
 class ActivitySmallestSerializer(serializers.ModelSerializer):
