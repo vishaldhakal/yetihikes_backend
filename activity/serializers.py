2e5c7a2c624309b8694b47d3aa13031fad764984
Ratish149 <ratish.shakya149@gmail.com>
2025-02-16 11:19:18 +0545

    asd

--- activity/serializers.py
+++ activity/serializers.py
@@ -139,10 +139,10 @@ class ActivitySmallSerializer(serializers.ModelSerializer):
         
 class LandingActivitySmallSerializer(serializers.ModelSerializer):
     group_price_available=serializers.SerializerMethodField()
-
+    activity_category=ActivityDestinationSerializer(many=True)
     class Meta:
         model = Activity
-        fields=('id','slug','activity_title','location','duration','price','heroImg','coverImg','priceSale','ratings','difficulty_level','group_price_available','activity_type')
+        fields=('id','slug','activity_title','activity_category','location','duration','price','heroImg','coverImg','priceSale','ratings','difficulty_level','group_price_available','activity_type')
         
     def get_group_price_available(self,obj):
         return ActivityPricing.objects.filter(activity=obj).exists()
